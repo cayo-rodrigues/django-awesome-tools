939d74d4645454b11691d48bbbd036cb64c0e07e
cayo-rodrigues <cayo.rodrigues1914@gmail.com>
2022-08-18 11:47:21 -0300

    fix: naming conflicts with existing pypi packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="django-utils",
+    name="dj-drf-utils",
     version="1.0.0.dev",
     description="Useful functions and classes for Django and Django Rest Framework",
     long_description=long_description,
@@ -15,7 +15,7 @@ setup(
     author="Cayo Rodrigues",
     author_email="cayo.rodrigues1914@gmail.com",
     license="MIT",
-    keywords="django utils serializers generic views viewsets mixins email login model manager custom action shortcut error",
+    keywords="django utils serializers generic views viewsets mixins email login model manager custom action shortcut error simple",
     packages=find_packages(include=["src", "src.*"]),
     install_requires=["djangorestframework"],
     python_requires=">=3.8",
