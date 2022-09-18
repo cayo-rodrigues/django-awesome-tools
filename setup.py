047ffd10f06977c3a8e779bc571e4fc271ca1096
cayo-rodrigues <cayo.rodrigues1914@gmail.com>
2022-09-18 19:21:11 -0300

    chore: update setup.py keywords

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="dj_drf_utils",
-    version="1.1.4",
+    version="1.2.0",
     description="Useful functions and classes for Django and Django Rest Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     author="Cayo Rodrigues",
     author_email="cayo.rodrigues1914@gmail.com",
     license="MIT",
-    keywords="django utils serializers generic views viewsets mixins email login model manager custom action shortcut error simple rest framework dj drf",
+    keywords="django utils serializers generic views viewsets mixins email login model manager custom action shortcut error simple rest framework dj drf admin hash password user",
     packages=find_packages(include=["dj_drf_utils", "dj_drf_utils.*"]),
     install_requires=["djangorestframework"],
     python_requires=">=3.8",
